f17bd755df238a17d306bb3fc39817e92fdf05ac
Josh Wu <joshwu@google.com>
2022-10-06 14:29:17 +0000

    sm7125-common: switch BT Audio to AIDL
    Signed-off-by: debdeep199x <debdeep199x@gmail.com>

--- common.mk
+++ common.mk
@@ -70,8 +70,8 @@ PRODUCT_PACKAGES += \
     libbthost_if
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
